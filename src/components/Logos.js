eedc42e2cfc7ab42c61f3e8eaa64b6f855a223e8
Evan Tan <tcyevan@gmail.com>
2022-03-06 16:43:37 +0800

    feat: add Arbitrum logo

--- src/components/Logos.js
+++ src/components/Logos.js
@@ -94,3 +94,139 @@ export const PolygonLogo = () => (
         />
     </svg>
 );
+
+export const ArbLogo = () => (
+    <svg
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        x="0px"
+        y="0px"
+        width="27.435px"
+        height="30px"
+        viewBox="0 0 27.435 30"
+        enable-background="new 0 0 470.287 514.251"
+    >
+        <g id="Background" />
+        <g id="Logos_and_symbols">
+            <g id="SYMBOL_VER_3" />
+            <g id="SYMBOL_VER_3_3_" />
+            <g id="SYMBOL_VER_4" />
+            <g id="SYMBOL_VER_4_1_">
+                <g id="SYMBOL_VER_4_3_" />
+            </g>
+            <g id="SYMBOL_VER_5_1_" />
+            <g id="off_2_1_" />
+            <g id="VER_3_1_">
+                <g id="SYMBOL_VER_2_1_" />
+            </g>
+            <g id="VER_3">
+                <g id="SYMBOL_VER_2" />
+            </g>
+            <g id="off_2" />
+            <g id="SYMBOL_VER_5" />
+            <g id="SYMBOL_VER_1" />
+            <g id="SYMBOL_VER_1_1_" />
+            <g id="SYMBOL_VER_1-1_3_" />
+            <g id="SYMBOL_VER_1-1_2_" />
+            <g id="SYMBOL_VER_1-1" />
+            <g id="SYMBOL_VER_1-1_1_">
+                <g id="_x31_-3" />
+                <g id="Symbol_-_Original_14_">
+                    <path
+                        fill="#2D374B"
+                        d="M16.984 13.853l2.08 -3.529l5.606 8.732l0.003 1.676l-0.018 -11.532c-0.013 -0.282 -0.163 -0.54 -0.402 -0.692L14.159 2.702c-0.236 -0.116 -0.536 -0.115 -0.771 0.004c-0.032 0.016 -0.062 0.033 -0.09 0.052l-0.035 0.022L3.465 8.458l-0.038 0.017c-0.049 0.022 -0.098 0.051 -0.145 0.084c-0.186 0.133 -0.309 0.33 -0.349 0.551c-0.006 0.033 -0.01 0.068 -0.012 0.102l0.015 9.397l5.222 -8.094c0.658 -1.073 2.09 -1.419 3.42 -1.4l1.561 0.041L3.943 23.904l1.084 0.624L14.333 9.172l4.113 -0.015L9.164 24.901l3.868 2.225l0.462 0.266c0.195 0.079 0.426 0.083 0.623 0.012l10.235 -5.932l-1.957 1.134L16.984 13.853zM17.777 25.283l-3.907 -6.132l2.385 -4.047l5.131 8.087L17.777 25.283z"
+                    />
+                    <path
+                        fill="#28A0F0"
+                        points="237.768,328.286 304.736,433.395 366.601,397.543 278.648,258.915 			"
+                        d="M13.871 19.151L17.777 25.283L21.386 23.191L16.255 15.104Z"
+                    />
+                    <path
+                        fill="#28A0F0"
+                        d="M24.673 20.732l-0.003 -1.676l-5.606 -8.732l-2.08 3.529l5.412 8.753l1.957 -1.134c0.192 -0.156 0.308 -0.385 0.32 -0.631L24.673 20.732z"
+                    />
+                    <path
+                        fill="#FFFFFF"
+                        d="M1.18 22.312l2.763 1.592l9.196 -14.748l-1.561 -0.041c-1.33 -0.019 -2.762 0.327 -3.42 1.4L2.936 18.61l-1.757 2.699V22.312z"
+                    />
+                    <path
+                        fill="#FFFFFF"
+                        points="316.202,156.964 245.692,157.22 86.17,420.462 141.928,452.565 157.091,426.849 			"
+                        d="M18.446 9.157L14.333 9.172L5.027 24.528L8.28 26.401L9.164 24.901Z"
+                    />
+                    <path
+                        fill="#96BEDC"
+                        d="M26.406 9.136c-0.034 -0.86 -0.5 -1.648 -1.23 -2.106L14.951 1.149c-0.722 -0.363 -1.623 -0.364 -2.346 0c-0.085 0.043 -9.944 5.761 -9.944 5.761c-0.136 0.065 -0.268 0.143 -0.391 0.232c-0.651 0.467 -1.048 1.192 -1.089 1.989v12.179l1.757 -2.699L2.921 9.213c0.002 -0.034 0.006 -0.068 0.012 -0.102c0.04 -0.221 0.163 -0.418 0.349 -0.552c0.046 -0.033 10.074 -5.837 10.106 -5.853c0.236 -0.118 0.535 -0.12 0.771 -0.004l10.094 5.806c0.239 0.152 0.388 0.41 0.402 0.692v11.641c-0.012 0.247 -0.11 0.476 -0.302 0.631l-1.957 1.134l-1.01 0.585l-3.609 2.091l-3.66 2.121c-0.197 0.071 -0.428 0.067 -0.623 -0.012l-4.33 -2.491l-0.885 1.5l3.891 2.24c0.129 0.073 0.243 0.138 0.337 0.191c0.146 0.082 0.245 0.136 0.28 0.153c0.277 0.134 0.675 0.213 1.033 0.213c0.329 0 0.649 -0.06 0.953 -0.179l10.63 -6.156c0.61 -0.473 0.969 -1.186 1.001 -1.958V9.136z"
+                    />
+                </g>
+                <g id="Symbol_-_Original_13_" />
+                <g id="Symbol_-_Original_6_" />
+                <g id="Symbol_-_Original_4_" />
+                <g id="One_color_version_-_White_3_">
+                    <g id="Symbol_-_Original_15_" />
+                </g>
+                <g id="One_color_version_-_White">
+                    <g id="Symbol_-_Original" />
+                </g>
+                <g id="Symbol_-_Monochromatic_3_">
+                    <g id="_x33__7_" />
+                </g>
+                <g id="Symbol_-_Monochromatic">
+                    <g id="_x33__3_" />
+                </g>
+                <g id="_x33__2_" />
+                <g id="_x33__1_" />
+                <g id="_x33_" />
+                <g id="Symbol_-_Original_10_" />
+                <g id="Symbol_-_Original_1_" />
+                <g id="Symbol_-_Original_2_" />
+                <g id="_x34__1_" />
+                <g id="Symbol_-_Monochromatic_2_">
+                    <g id="_x33__6_" />
+                </g>
+                <g id="One_color_version_-_White_2_">
+                    <g id="Symbol_-_Original_11_" />
+                </g>
+                <g id="Symbol_-_Original_5_">
+                    <g id="Symbol_-_Original_12_" />
+                </g>
+                <g id="One_color_version_-_White_1_">
+                    <g id="Symbol_-_Original_9_" />
+                </g>
+            </g>
+            <g id="SYMBOL_VER_1_2_">
+                <g id="SYMBOL_VER_2_4_" />
+                <g id="SYMBOL_VER_2-1-1_1_" />
+                <g id="SYMBOL_VER_2-2-1_1_" />
+                <g id="SYMBOL_VER_2-3-1_4_" />
+                <g id="New_Symbol_1_">
+                    <g id="SYMBOL_VER_2-3-1_3_" />
+                </g>
+                <g id="New_Symbol">
+                    <g id="SYMBOL_VER_2-3-1_1_" />
+                </g>
+            </g>
+            <g id="SYMBOL_VER_2_2_" />
+            <g id="SYMBOL_VER_4_2_" />
+            <g id="SYMBOL_VER_3_2_" />
+            <g id="SYMBOL_VER_3_1_" />
+            <g id="SYMBOL_VER_1-1-1_1_" />
+            <g id="SYMBOL_VER_1-1-1" />
+            <g id="SYMBOL_VER_1-1-1_2_2_" />
+            <g id="SYMBOL_VER_1-1-1_2" />
+            <g id="SYMBOL_VER_1-1-1_2_1_" />
+            <g id="Symbol_-_Original_7_" />
+            <g id="Symbol_-_Original_8_" />
+            <g id="SYMBOL_VER_2-1-1" />
+            <g id="SYMBOL_VER_2-2-1" />
+            <g id="SYMBOL_VER_2-3-1" />
+            <g id="SYMBOL_VER_5-1_1_" />
+            <g id="SYMBOL_VER_5-1" />
+            <g id="SYMBOL_VER_5-2_1_" />
+            <g id="SYMBOL_VER_5-2" />
+            <g id="Symbol_-_Monochromatic_1_">
+                <g id="_x33__4_" />
+            </g>
+        </g>
+    </svg>
+);
