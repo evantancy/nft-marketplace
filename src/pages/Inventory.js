10528a68a1b6805bd8402c045dca04b116c0b981
Evan Tan <tcyevan@gmail.com>
2022-03-09 00:22:29 +0800

    feat: support rendering videos

--- src/pages/Inventory.js
+++ src/pages/Inventory.js
@@ -14,22 +14,28 @@ import {
     Container,
     Image,
     CardGroup,
+    Video,
 } from "react-bootstrap";
 import { supportedChains } from "../utils/Networks";
+// import { loadImgURL } from "../utils/IpfsUtils";
 
 const Inventory = () => {
     const RenderCard = (data, symbol, index) => {
-        const name =
-            data?.name?.length > 20 ? data?.name?.substring(0, 16) : data?.name;
+        // const name =
+        //     data?.name?.length > 20 ? data?.name?.substring(0, 16) : data?.name;
+        const name = data?.name;
         const address =
             data?.token_address.substring(0, 6) +
             "...." +
             data?.token_address.slice(-4);
-        const tokenId = "#" + data?.token_id;
+        const tokenId =
+            data?.token_id?.length > 10
+                ? "#" + data?.token_id?.substring(0, 10)
+                : "#" + data?.token_id;
         const type = data?.contract_type;
         // only show symbol if price specified i.e. listed for sale
         // TODO: fetch price from contract
-        const price = 0;
+        const price = "";
         const currencySymbol = price > 0 ? symbol : "";
 
         const imgContainerStyle = {
@@ -79,21 +85,42 @@ const Inventory = () => {
             marginBottom: 0,
         };
 
+        const RenderVideo = () => (
+            <video
+                src={data?.image}
+                style={{ maxWidth: "14rem" }}
+                autoPlay
+                loop
+                muted
+            />
+        );
+
+        const RenderImage = () => (
+            <Card.Img
+                variant="top"
+                as={Image}
+                src={data?.image}
+                style={{ maxWidth: "14rem" }}
+                rounded
+                fluid
+            />
+        );
+
+        // const RenderIpfsImage = async () =>
+        //     loadImgURL("QmUyAmor5hEMF5dtMksk7EE3ta2QgBTKH3xSLtoTDHwhGZ");
+
+        // TODO: load IPFS images quickly
         return (
             <Card className="mb-2 mx-1" key={index} style={cardStyle}>
                 <Container className="" style={imgContainerStyle}>
-                    <Card.Img
-                        variant="top"
-                        as={Image}
-                        src={data?.image}
-                        // className="img-fluid"
-                        rounded
-                        fluid
-                    />
+                    {data?.image.search("mp4") >= 0
+                        ? RenderVideo()
+                        : RenderImage()}
                 </Container>
                 <Card.Body style={cardTitleStyle}>
                     <Card.Title className="float-start" style={cardTextStyle}>
-                        {name} {tokenId}
+                        <p style={cardInfoStyle}>{name}</p>
+                        <p style={cardInfoStyle}>{tokenId}</p>
                     </Card.Title>
                     <Card.Title className="float-end" style={cardTextStyle}>
                         {price} {currencySymbol}
@@ -162,9 +189,11 @@ const Inventory = () => {
             : "";
 
     return (
-        <div className="container">
-            <h1>Inventory</h1>
-            <CardGroup>{cards}</CardGroup>
+        <div style={{ backgroundColor: "#3C4046", minHeight: "100vh" }}>
+            <div className="container">
+                <h1>Your NFTs</h1>
+                <CardGroup>{cards}</CardGroup>
+            </div>
         </div>
     );
 };
