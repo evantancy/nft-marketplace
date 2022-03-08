48be57196d1c0ee2de62182154024ce63f315bd0
Evan Tan <tcyevan@gmail.com>
2022-03-08 14:06:09 +0800

    fix: display all items in nice grid

--- src/pages/Inventory.js
+++ src/pages/Inventory.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
     useNFTBalances,
     useChain,
@@ -15,35 +15,42 @@ import {
     Container,
     Row,
     Col,
+    CardGroup,
 } from "react-bootstrap";
 import { supportedChains } from "../utils/Networks";
 import { debounce } from "lodash";
 
-// render a card for a single token
-const RenderCard = (data, index) => {
-    const name =
-        data?.name?.length > 12 ? data?.name.substring(0, 10) : data?.name;
-    const address =
-        data?.token_address.substring(0, 6) +
-        "...." +
-        data?.token_address.slice(-4);
-    const tokenId = "#" + data?.token_id;
-    const type = data?.contract_type;
-    // only show symbol if price specified i.e. listed for sale
-    const price = 0;
-    const symbol = "";
+const Inventory = () => {
+    // render a card for a single token
 
-    return (
-        <div>
-            {/* TODO: Fix unique key error */}
-            <Card key={index} style={{ width: "18rem" }}>
+    // render a single card
+    const RenderCard = (data, symbol, index) => {
+        const name =
+            data?.name?.length > 12 ? data?.name?.substring(0, 10) : data?.name;
+        const address =
+            data?.token_address.substring(0, 6) +
+            "...." +
+            data?.token_address.slice(-4);
+        const tokenId = "#" + data?.token_id;
+        const type = data?.contract_type;
+        // only show symbol if price specified i.e. listed for sale
+        // TODO: fetch price from contract
+        const price = 0;
+        const currencySymbol = price ? symbol : "";
+
+        return (
+            <Card
+                className="mb-2 mx-2"
+                key={index}
+                style={{ minWidth: "18rem", maxWidth: "18rem" }}
+            >
                 <Card.Img variant="top" src="" />
-                <Card.Body style={{}}>
+                <Card.Body>
                     <Card.Title className="float-start">
                         {name} {tokenId}
                     </Card.Title>
                     <Card.Title className="float-end">
-                        {price} {symbol}
+                        {price} {currencySymbol}
                     </Card.Title>
                 </Card.Body>
                 <ListGroup
@@ -53,8 +60,6 @@ const RenderCard = (data, index) => {
                     <ListGroupItem className="border-0">
                         <p className="float-start">Contract Address</p>
                         <p className="float-end">{address}</p>
-                    </ListGroupItem>
-                    <ListGroupItem className="border-0">
                         <p className="float-start">Contract Type</p>
                         <p className="float-end">{type}</p>
                     </ListGroupItem>
@@ -71,17 +76,16 @@ const RenderCard = (data, index) => {
                     </ListGroupItem>
                 </ListGroup>
             </Card>
-        </div>
-    );
-};
+        );
+    };
 
-const Inventory = () => {
     const { switchNetwork, chainId, chain } = useChain();
     const { Moralis } = useMoralis();
     const { account } = useMoralisWeb3Api();
     const { getNFTBalances, data, error, isLoading, isFetching } =
         useNFTBalances();
-
+    const [cardRows, setCardRows] = useState(0);
+    const [cardCols, setCardCols] = useState(0);
     const _symbol = supportedChains.find((item) => item.id === chainId)?.symbol;
 
     // dynamically get rows and cols
@@ -91,18 +95,23 @@ const Inventory = () => {
         const cardWidth = 288;
         const cols = width / cardWidth;
         const rows = numItems / cols;
-        return { rows, cols };
+        // return { rows, cols };
+        setCardRows(rows);
+        setCardCols(cols);
     }
+
     console.log(data?.result);
 
-    const dims = calcRowCol(data?.total);
     // render outside to dynamically adjust
-    const cards = data !== null ? data?.result.map(RenderCard) : "";
+    const cards =
+        data !== null
+            ? data?.result.map((item) => RenderCard(item, _symbol))
+            : "";
 
     return (
         <div className="container">
             <h1>Inventory</h1>
-            <Row className="row-cols-1 row-cols-md-4 g-4">{cards}</Row>
+            <CardGroup>{cards}</CardGroup>
         </div>
     );
 };
