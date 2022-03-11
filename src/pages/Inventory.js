cbca8d5d73075a88bbb13d5e1c0a8165cf6bba89
Evan Tan <tcyevan@gmail.com>
2022-03-11 13:56:20 +0800

    refactor: add additional checks before loading

--- src/pages/Inventory.js
+++ src/pages/Inventory.js
@@ -7,24 +7,30 @@ import { CustomCard } from "../components/Card";
 const Inventory = () => {
     const { isAuthenticated } = useMoralis();
     const { chainId } = useChain();
-    const { data } = useNFTBalances();
+    const { data, error, isLoading, isFetching } = useNFTBalances();
 
     // const { getNFTBalances, error, isLoading, isFetching } = useNFTBalances();
     const _symbol = supportedChains.find((item) => item.id === chainId)?.symbol;
 
-    console.log("Fetched data\n", data?.result);
+    console.log("Fetched data\n", !isLoading && !isFetching);
 
     return (
         <div style={{ backgroundColor: "#3C4046", minHeight: "100vh" }}>
             <div className="container">
                 <h1>Your NFTs</h1>
                 <CardGroup style={{ justifyContent: "center" }}>
-                    {data !== null && isAuthenticated
+                    {data !== null &&
+                    isAuthenticated &&
+                    !isLoading &&
+                    !isFetching
                         ? data?.result.map((item, index) => (
                               <CustomCard
                                   data={item}
                                   symbol={_symbol}
                                   key={index}
+                                  listButton
+                                  transferButton
+                                  cardInfo
                               />
                           ))
                         : ""}
