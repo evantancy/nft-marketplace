f91bddbc35a29e69ebeabf67abd9f009b6bc54d3
Evan Tan <tcyevan@gmail.com>
2022-03-12 23:46:39 +0800

    refactor: change link color + link hover color

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 // import { useMoralis } from "react-moralis";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import Explore from "./pages/Explore";
 import Inventory from "./pages/Inventory";
 import Transaction from "./pages/Transaction";
 import ErrorPage from "./pages/ErrorPage";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    Link,
+    Outlet,
+} from "react-router-dom";
 import { Nav, Navbar, Container } from "react-bootstrap";
 import { ConnectButton } from "web3uikit";
 import ChainSelector from "./components/ChainSelector";
@@ -20,9 +26,10 @@ const Header = () => {
                 style={{ marginBottom: "10" }}
             >
                 <Container>
-                    <Navbar.Brand as={Link} to="/">
+                    {/* <Navbar.Brand as={Link} to="/">
                         NFT Marketplace
-                    </Navbar.Brand>
+                    </Navbar.Brand> */}
+                    <Navbar.Brand>NFT Marketplace</Navbar.Brand>
                     <Navbar.Toggle
                         className="me-auto"
                         aria-controls="responsive-navbar-nav"
@@ -51,17 +58,27 @@ const Header = () => {
     );
 };
 
+const Layout = () => {
+    return (
+        <div>
+            <Header />
+            <Outlet />
+        </div>
+    );
+};
+
 function App() {
     return (
         <Router>
-            <Header />
             {/* configure routes */}
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/explore" element={<Explore />} />
-                <Route path="/inventory" element={<Inventory />} />
-                <Route path="/transactions" element={<Transaction />} />
-                <Route path="*" element={<ErrorPage />} />
+                {/* <Route path="/" element={<Home />} /> */}
+                <Route path="/" element={<Layout />}>
+                    <Route path="/explore" element={<Explore />} />
+                    <Route path="/inventory" element={<Inventory />} />
+                    <Route path="/transactions" element={<Transaction />} />
+                    <Route path="*" element={<ErrorPage />} />
+                </Route>
             </Routes>
         </Router>
     );
